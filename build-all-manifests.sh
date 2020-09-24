4fad750dec28d7c40f74549b78ac10db55f8ddfe
Bart van benthem <bart.vanbenthem@ns.nl>
2020-09-24 19:48:27 +0200

    manifest folders setup

--- build-all-manifests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./k8s-manifestgen clusters/all-clusters/raw/values/allclusters.yaml clusters/all-clusters/raw/templates/allclusters.yaml clusters/all-clusters/raw/generated
--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func GenerateAllClustersManifest() string {
 	cmd := exec.Command("./k8s-manifestgen",
 		"clusters/all-clusters/raw/values/allclusters.yaml",
 		"clusters/all-clusters/raw/templates/allclusters.yaml",
-		"clusters/all-clusters/raw/generated")
+		"clusters/all-clusters/raw/manifest")
 	stdoutStderr, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Printf("Error: %v\n", err)
