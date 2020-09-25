23fa47a9427c2e234f684a02afbbaa6aa12daf90
Bart van benthem <bart.vanbenthem@ns.nl>
2020-09-25 14:31:04 +0200

    update main

--- main.go
+++ main.go
@@ -1,23 +1,19 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/bartvanbenthem/k8s-manifestgen/client"
 )
 
 func main() {
-	ac := GenerateClusterAllManifest("clusters/cluster-all/values/allclusters.yaml",
+	GenerateClusterAllManifest("clusters/cluster-all/values/allclusters.yaml",
 		"clusters/cluster-all/templates/allclusters.yaml",
 		"clusters/cluster-all/manifest")
-	fmt.Println(ac)
 }
 
 func GenerateClusterSpecificManifest() {}
 func GenerateClusterHelmManifest()     {}
 
-func GenerateClusterAllManifest(valuesFile, templateFile, outputFolder string) string {
-	var cmg client.ManifestGenClient
+func GenerateClusterAllManifest(valuesFile, templateFile, outputFolder string) {
 	//build a foreach loop to check all value and template files within their specific dir
-	cmg.GenerateManifestFromValues(valuesFile, templateFile, outputFolder)
+	client.GenerateManifestFromValues(valuesFile, templateFile, outputFolder)
 }
