89b5258bdcedf45e1effe1acb0732a02f52f1295
Bart van benthem <bart.vanbenthem@ns.nl>
2020-09-25 15:06:42 +0200

    read all files function

--- main.go
+++ main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
 	"github.com/bartvanbenthem/k8s-manifestgen/client"
 )
 
 func main() {
-	GenerateClusterAllManifest("clusters/cluster-all/values/allclusters.yaml",
-		"clusters/cluster-all/templates/allclusters.yaml",
+	GenerateClusterAllManifest("clusters/cluster-all/values",
+		"clusters/cluster-all/templates",
 		"clusters/cluster-all/manifest")
 }
 
 func GenerateClusterSpecificManifest() {}
 func GenerateClusterHelmManifest()     {}
 
-func GenerateClusterAllManifest(valuesFile, templateFile, outputFolder string) {
-	//build a foreach loop to check all value and template files within their specific dir
-	client.GenerateManifestFromValues(valuesFile, templateFile, outputFolder)
+func GenerateClusterAllManifest(valuesPath, templatePath, outputFolder string) {
+
+	valueFiles := ReadFileNames(valuesPath)
+	templateFiles := ReadFileNames(templatePath)
+
+	for _, val := range valueFiles {
+		for _, tmpl := range templateFiles {
+			if tmpl.fileName == val.fileName {
+				client.GenerateManifestFromValues(val.pathAndFile, tmpl.pathAndFile, outputFolder)
+			}
+		}
+	}
+}
+
+type FileInfo struct {
+	fileName    string
+	filePath    string
+	pathAndFile string
+}
+
+func ReadFileNames(path string) []FileInfo {
+	var file FileInfo
+	var files []FileInfo
+
+	fs, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range fs {
+		file.fileName = f.Name()
+		file.filePath = path
+		file.pathAndFile = fmt.Sprintf("%v/%v", path, f.Name())
+		files = append(files, file)
+	}
+
+	return files
 }
